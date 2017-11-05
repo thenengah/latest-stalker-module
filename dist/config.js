bd0f19163550e78615a5a78244d7c38981b2ef0a
thenengah <thenengah@gmail.com>
2017-11-05 17:55:13 +0800

    removed dist; only needed for npm

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = {
-  method: 'post',
-  uri: 'https://latest-stalker.herokuapp.com/api/stalk'
-};
